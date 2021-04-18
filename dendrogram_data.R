c8190e1b24010a7410291c28f7f0666109345fcd
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-04-18 19:15:27 +0200

    updated data

--- dendrogram_data.R
+++ dendrogram_data.R
@@ -35,57 +35,104 @@ dendrogram_data <- function(x, type = c("rectangle", "triangle"), ...) {
   xl. <- c(x1 - 1 / 2, x2 + 1 / 2)
   yl. <- c(0, yTop)
   
-  ret <- gg.plotNode(x1, x2, x,
-                     type = type, center = center,
-                     ddsegments = NULL, ddlabels = NULL)
-  names(ret$segments) <- c("x", "y", "xend", "yend")
-  names(ret$labels) <- c("x", "y", "label")
-  ret
+  dd_env <- new.env()
+  assign(x = "ddlabels", value = NULL, inherits = FALSE, envir = dd_env)
+  assign(x = "ddsegments", value = NULL, inherits = FALSE, envir = dd_env)
+  
+  plotNode_newR(x1, x2, x,
+                type = type, center = center,
+                dd_env = dd_env)
+  res <- NULL
+  res[["segments"]] <- get("ddsegments", envir = dd_env)
+  res[["labels"]] <- get("ddlabels", envir = dd_env)
+  
+  names(res[["segments"]]) <- c("x", "y", "xend", "yend")
+  names(res[["labels"]]) <- c("x", "y", "label")
+  res
 }
 
-gg.plotNode <- function(x1, x2, subtree, type, center, ddsegments = NULL, ddlabels = NULL) {
-  inner <- !is.leaf(subtree) && x1 != x2
-  yTop <- attr(subtree, "height")
+plotNode_newR <- function(x1, x2, subtree, type, center, dd_env)
+{
+  wholetree <- subtree
+  depth <- 0L
+  llimit <- list()
+  KK <- integer()
+  kk <- integer()
   
-  bx <- plotNodeLimit(x1, x2, subtree, center)
-  xTop <- bx$x
-  
-  if (is.leaf(subtree)) {
-    ddlabels <- rbind(ddlabels, data.frame(x = xTop, y = 0, text = asTxt(attr(subtree, "label"))))
-  }
-  else if (inner) {
-    for (k in seq_along(subtree)) {
-      child <- subtree[[k]]
-      yBot <- attr(child, "height")
-      
-      if (is.null(yBot)) {
-        yBot <- 0
-      }
-      xBot <- if (center) {
-        mean(bx$limit[k:(k + 1)])
-      } else {
-        bx$limit[k] + .midDend(child)
-      }
-      
-      if (type == "triangle") {
-        # *************************
-        ddsegments <- rbind(ddsegments, segmentsHV(xTop, yTop, xBot, yBot))
-      }
-      else {
-        # *************************
-        ddsegments <- rbind(ddsegments, segmentsHV(xTop, yTop, xBot, yTop))
-        ddsegments <- rbind(ddsegments, segmentsHV(xBot, yTop, xBot, yBot))
-      }
-      
-      plotNode_result <- gg.plotNode(bx$limit[k], bx$limit[k + 1],
-                                     subtree = child,
-                                     type, center, ddsegments, ddlabels)
-      ddsegments <- plotNode_result$segments
-      ddlabels <- plotNode_result$labels
+  repeat {
+    inner <- !is.leaf(subtree) && x1 != x2
+    yTop <- attr(subtree, "height")
+    bx <- plotNodeLimit(x1, x2, subtree, center)
+    xTop <- bx$x
+    depth <- depth + 1L
+    llimit[[depth]] <- bx$limit
+    
+    asTxt <- function(x) # to allow 'plotmath' labels:
+      if(is.character(x) || is.expression(x) || is.null(x)) x else as.character(x)
+    
+    if (is.leaf(subtree)) {
+      ddlabels_local <- rbind(get("ddlabels", envir = dd_env), 
+                              data.frame(x = xTop, y = 0, text = asTxt(attr(subtree, "label"))))
+      assign(x = "ddlabels", value = ddlabels_local, inherits = FALSE, envir = dd_env)
     }
-  }
-  list(segments = ddsegments, labels = ddlabels)
+    else if (inner) {
+      for (k in seq_along(subtree)) {
+        child <- subtree[[k]]
+        ## draw lines to the children and draw them recursively
+        yBot <- attr(child, "height")
+        
+        if (is.null(yBot)) {
+          yBot <- 0
+        }
+        xBot <- if (center) {
+          mean(bx$limit[k:(k + 1)])
+        } else {
+          bx$limit[k] + .midDend(child)
+        }
+        
+        if (type == "triangle") {
+          ddsegments_local <- rbind(get("ddsegments", envir = dd_env), 
+                                    segmentsHV(xTop, yTop, xBot, yBot))
+          assign(x = "ddsegments", value = ddsegments_local, 
+                 inherits = FALSE, envir = dd_env)
+        } else {
+          ddsegments_local <- rbind(get("ddsegments", envir = dd_env), 
+                                    segmentsHV(xTop, yTop, xBot, yBot))
+          ddsegments_local <- rbind(ddsegments_local, segmentsHV(xTop, yTop, xBot, yTop))
+          ddsegments_local <- rbind(ddsegments_local, segmentsHV(xBot, yTop, xBot, yBot))
+          assign(x = "ddsegments", value = ddsegments_local, 
+                 inherits = FALSE, envir = dd_env)
+        }
+      }
+    }
+    
+    if (inner && length(subtree)) {
+      KK[depth] <- length(subtree)
+      if (storage.mode(kk) != storage.mode(KK))
+        storage.mode(kk) <- storage.mode(KK)
+      
+      ## go to first child
+      kk[depth] <- 1L
+      x1 <- bx$limit[1L]
+      x2 <- bx$limit[2L]
+      subtree <- subtree[[1L]]
+    }
+    else {
+      repeat {
+        depth <- depth - 1L
+        if (!depth || kk[depth] < KK[depth]) break
+      }
+      if (!depth) break
+      length(kk) <- depth
+      kk[depth] <- k <- kk[depth] + 1L
+      x1 <- llimit[[depth]][k]
+      x2 <- llimit[[depth]][k + 1L]
+      subtree <- wholetree[[kk]]
+    }
+  } ## repeat
+  invisible()
 }
+
 
 segmentsHV <- function(x0, y0, x1, y1) {
   data.frame(x0, y0, x1, y1) # AdV
--- example.R
+++ example.R
@@ -5,15 +5,15 @@ source("./dendrogram_data.R")
 #strain_dendrogram_data <- dendrogram_data(dendro)
 hc <- hclust(dist(USArrests), "ave")
 hcdata <- dendrogram_data(hc)
-
-hc[["merge"]]
-hc[["order"]]
-hc[["labels"]]
-hc[["height"]]
-
-lapply(nrow(hc[["merge"]]):1, function(i) {
-  data.frame(x = 1, xend = 2, y = hc[["height"]][i], yend = hc[["height"]][i])
-})
-
-ggplot(hcdata[[1]][1L:10,], aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment()
+# 
+# hc[["merge"]]
+# hc[["order"]]
+# hc[["labels"]]
+# hc[["height"]]
+# 
+# lapply(nrow(hc[["merge"]]):1, function(i) {
+#   data.frame(x = 1, xend = 2, y = hc[["height"]][i], yend = hc[["height"]][i])
+# })
+# 
+# ggplot(hcdata[[1]][1L:10,], aes(x = x, y = y, xend = xend, yend = yend)) +
+#   geom_segment()
